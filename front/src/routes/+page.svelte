9c3b1d5fb80b118ce4b2ccc8b8f52d20af1f479b
myuceller <myuceller@github.com>
2025-03-28 16:31:24 +0900

    yellow box
    almost done design, no function work yet

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/ingame">ingame</a>
+</nav>
+
+<h1>Home</h1>
+<p>this is the Home page.</p>
---
+++ front/src/routes/ingame/+page.svelte
@@ -0,0 +1,191 @@
+<div class="main">
+  <div class="yellowSection">
+    <svg class="arrows" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 450 450">
+      <defs>
+        <marker id="arrowhead" markerWidth="5" markerHeight="7" refX="5" refY="3.5" orient="auto">
+          <polygon points="0 0, 5 3.5, 0 7" fill="black"/>
+        </marker>
+      </defs>
+      
+      <!-- 가로 화살표 (→) -->
+      <line x1="50" y1="60" x2="384" y2="60" marker-end="url(#arrowhead)" />
+      <line x1="50" y1="136" x2="384" y2="136" marker-end="url(#arrowhead)" />
+      <line x1="50" y1="216" x2="384" y2="216" marker-end="url(#arrowhead)" />
+      <line x1="50" y1="294" x2="384" y2="294" marker-end="url(#arrowhead)" />
+
+      <line x1="50" y1="60" x2="50" y2="364" marker-end="url(#arrowhead)" />
+      <line x1="138" y1="60" x2="138" y2="364" marker-end="url(#arrowhead)" /> 
+      <line x1="224" y1="60" x2="224" y2="364" marker-end="url(#arrowhead)" />
+      <line x1="310" y1="60" x2="310" y2="364" marker-end="url(#arrowhead)" /> 
+
+      <!-- 대각선 화살표 (↘) -->
+      <line x1="50" y1="60" x2="384" y2="364" marker-end="url(#arrowhead)" />
+    </svg>
+
+      
+    <div class="whiteboxwrapper">
+      <div class="whitebox">3</div>
+      <div class="whitebox">6</div>
+      <div class="whitebox">5</div>
+      <div class="whitebox">X</div>
+      <div class="whitebox">2</div>
+      <div class="whitebox">1</div>
+      <div class="whitebox">X</div>
+      <div class="whitebox">5</div>
+      <div class="whitebox">1</div>
+      <div class="whitebox">X</div>
+      <div class="whitebox">2</div>
+      <div class="whitebox">4</div>
+      <div class="whitebox">X</div>
+      <div class="whitebox">3</div>
+      <div class="whitebox">4</div>
+      <div class="whitebox">6</div>
+    </div>
+    <div class="bonusbox-container">
+      <div class="grid-container">
+        <div class="bonusbox">X</div>
+        <div class="bonusbox , orange">4</div>
+        <div class="bonusbox , green">X</div>
+        <div class="bonusbox , fox">fox</div>
+      </div>
+    </div>
+    <div class="stars-container">
+      <svg class="scroebox" width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.1213 2.00372C27.8367 1.68333 27.4286 1.5 27 1.5C26.5714 1.5 26.1633 1.68333 25.8787 2.00372L16.105 13.0043L2.62258 18.9002C2.22991 19.072 1.92944 19.4034 1.797 19.811C1.66456 20.2187 1.7128 20.6634 1.92955 21.0332L9.37149 33.7278L10.8126 48.3723C10.8546 48.7988 11.077 49.187 11.4237 49.4389C11.7704 49.6909 12.2083 49.7824 12.627 49.6905L27 46.5357L41.373 49.6905C41.7917 49.7824 42.2296 49.6909 42.5763 49.4389C42.923 49.187 43.1455 48.7988 43.1874 48.3723L44.6285 33.7278L52.0704 21.0332C52.2872 20.6634 52.3354 20.2187 52.203 19.811C52.0706 19.4034 51.7701 19.072 51.3774 18.9002L37.895 13.0043L28.1213 2.00372Z" 
+              fill="white" stroke="black" stroke-width="3" stroke-linejoin="round"/>
+      
+        <!-- 텍스트 추가 -->
+        <text x="27" y="30" font-size="25" fill="black" text-anchor="middle">10</text>
+      </svg>
+      
+
+      <svg class="scroebox" width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.1213 2.00372C27.8367 1.68333 27.4286 1.5 27 1.5C26.5714 1.5 26.1633 1.68333 25.8787 2.00372L16.105 13.0043L2.62258 18.9002C2.22991 19.072 1.92944 19.4034 1.797 19.811C1.66456 20.2187 1.7128 20.6634 1.92955 21.0332L9.37149 33.7278L10.8126 48.3723C10.8546 48.7988 11.077 49.187 11.4237 49.4389C11.7704 49.6909 12.2083 49.7824 12.627 49.6905L27 46.5357L41.373 49.6905C41.7917 49.7824 42.2296 49.6909 42.5763 49.4389C42.923 49.187 43.1455 48.7988 43.1874 48.3723L44.6285 33.7278L52.0704 21.0332C52.2872 20.6634 52.3354 20.2187 52.203 19.811C52.0706 19.4034 51.7701 19.072 51.3774 18.9002L37.895 13.0043L28.1213 2.00372Z" fill="white" stroke="black" stroke-width="3" stroke-linejoin="round"/>
+        <text x="27" y="30" font-size="25" fill="black" text-anchor="middle">14</text>
+      </svg>
+    
+      <svg class="scroebox" width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.1213 2.00372C27.8367 1.68333 27.4286 1.5 27 1.5C26.5714 1.5 26.1633 1.68333 25.8787 2.00372L16.105 13.0043L2.62258 18.9002C2.22991 19.072 1.92944 19.4034 1.797 19.811C1.66456 20.2187 1.7128 20.6634 1.92955 21.0332L9.37149 33.7278L10.8126 48.3723C10.8546 48.7988 11.077 49.187 11.4237 49.4389C11.7704 49.6909 12.2083 49.7824 12.627 49.6905L27 46.5357L41.373 49.6905C41.7917 49.7824 42.2296 49.6909 42.5763 49.4389C42.923 49.187 43.1455 48.7988 43.1874 48.3723L44.6285 33.7278L52.0704 21.0332C52.2872 20.6634 52.3354 20.2187 52.203 19.811C52.0706 19.4034 51.7701 19.072 51.3774 18.9002L37.895 13.0043L28.1213 2.00372Z" fill="white" stroke="black" stroke-width="3" stroke-linejoin="round"/>
+        <text x="27" y="30" font-size="25" fill="black" text-anchor="middle">16</text>
+      </svg>
+    
+      <svg class="scroebox" width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.1213 2.00372C27.8367 1.68333 27.4286 1.5 27 1.5C26.5714 1.5 26.1633 1.68333 25.8787 2.00372L16.105 13.0043L2.62258 18.9002C2.22991 19.072 1.92944 19.4034 1.797 19.811C1.66456 20.2187 1.7128 20.6634 1.92955 21.0332L9.37149 33.7278L10.8126 48.3723C10.8546 48.7988 11.077 49.187 11.4237 49.4389C11.7704 49.6909 12.2083 49.7824 12.627 49.6905L27 46.5357L41.373 49.6905C41.7917 49.7824 42.2296 49.6909 42.5763 49.4389C42.923 49.187 43.1455 48.7988 43.1874 48.3723L44.6285 33.7278L52.0704 21.0332C52.2872 20.6634 52.3354 20.2187 52.203 19.811C52.0706 19.4034 51.7701 19.072 51.3774 18.9002L37.895 13.0043L28.1213 2.00372Z" fill="white" stroke="black" stroke-width="3" stroke-linejoin="round"/>
+        <text x="27" y="30" font-size="25" fill="black" text-anchor="middle">20</text>
+      </svg>
+    </div>
+    <div class="diagnalbonusbox">
+      <div class="bonusbox , bonus"></div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .yellowSection {
+    position: absolute;
+    display: flex;
+    width: 454px;
+    height: 431px;
+    background-color: #FFA710;
+    border-radius: 10px;
+    border: 2px solid black;
+  }
+
+  .whiteboxwrapper {
+    width: 318px;
+    height: 293px;
+    margin-top: 25px;
+    margin-left: 25px;
+    display: grid;
+    grid-template-columns: repeat(4, 68px);
+    grid-template-rows: repeat(4, 63px);
+    gap: 12px 15px;
+    position: relative;
+  }
+
+  .whitebox {
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 64px;
+    height: 59px;
+    border: 2px solid black;
+    border-radius: 10px;
+    text-align: center;
+    z-index: 3;
+  }
+
+  /* 🔥 화살표 스타일 */
+  .arrows {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2; /* 화살표가 박스 뒤로 가게 설정 */
+    pointer-events: none; /* 클릭 방지 */
+  }
+
+  .arrows line {
+    stroke: black;
+    stroke-width: 3;
+    fill: none;
+  }
+  .bonusbox-container{
+    width: 50px;
+    height: 200px;
+    margin-left:40px;
+    margin-top:33px;
+    display: flex;
+  }
+
+  .grid-container{
+    position:absolute;
+    display: grid;
+    grid-template-columns: 1fr; /* 세로로 하나의 열로 배치 */
+    grid-template-rows: repeat(4,48px);
+    gap: 27px; /* 박스들 사이의 간격 */
+  }
+  .bonusbox{
+    width: 48px;
+    height: 48px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: #2D5CC7;
+    border: 3px solid #FFFFFF;
+    border-radius: 10px;
+    z-index: 5;
+  }
+  .orange{
+    background-color: #FF6337;
+  }
+  .green{
+    background-color: #2EB573;
+  }
+  .fox{
+    background-color: #DF1919;
+  }
+  .bonus{
+    background-color: #000000;
+    color:#FFFFFF;
+  }
+  .diagnalbonusbox{
+    display: flex;
+    width: 54px;
+    height: 54px;
+    position: absolute;
+    top: 350px; /* 화살표 끝부분 아래에 위치하도록 조정 */
+    left: 383px; /* 화살표 끝부분에서 조금 떨어진 위치 */
+  }
+  .stars-container {
+    position: absolute;
+    display: flex;
+    bottom: 20px;
+    margin-left:32px;
+    margin-top:40px;
+    gap:29px;
+  }
+
+</style>
